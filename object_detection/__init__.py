a90fe3064d061041fb32b149fc7fea9fd05e325b
Jack Lim <jacklim.tkl@gmail.com>
2018-08-28 11:51:08 -0400

    init

---
+++ object_detection/__init__.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+#
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from flask import Flask, redirect, url_for, session, request, render_template,send_file
+from werkzeug.utils import secure_filename
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+from PIL import Image
+from collections import defaultdict
+from io import StringIO
+import json
+import time
+sys.path.append("..")
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+UPLOAD_FOLDER = '/uploads'
+PATH_TO_FROZEN_GRAPH = '/Users/tzekeonglim/tensorflow-for-poets-2/DeepFashionV2.0/training/fine_tuned_model/frozen_inference_graph.pb'
+PATH_TO_LABELS = '/Users/tzekeonglim/tensorflow-for-poets-2/DeepFashionV2.0/data/label_map.pbtxt'
+NUM_CLASSES = 27
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+PATH_TO_TEST_IMAGES_DIR = 'test_images'
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+def run_inference_for_single_image(image, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+          print(tensor_dict[key])
+      if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+      # Run inference
+      output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: np.expand_dims(image, 0)})
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.uint8)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+  return output_dict
+
+
+app = Flask(__name__)
+# sslify = SSLify(app)
+app.debug = True
+app.secret_key = 'development'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/send', methods=['POST'])
+def send():
+#   arr = json.loads(request.data)['image']['data']['data']
+#   filename = json.loads(request.data)['image']['name']
+#   f = open(os.path.join('./uploads',filename), 'wb')
+#   f.write(bytearray(arr))
+#   f.close()
+
+#   file_name = os.path.join('./uploads',filename)
+  print('hello')
+  image = request.files['image']
+  filename = secure_filename(image.filename)
+  image.save(os.path.join('./uploads', filename))
+  print(1)
+#   image_path = TEST_IMAGE_PATHS[0]
+  image_path = os.path.join('./uploads',filename)
+  print(image_path)
+  image = Image.open(image_path)
+  image_np = load_image_into_numpy_array(image)
+  output_dict = run_inference_for_single_image(image_np, detection_graph)
+  print(output_dict)
+  vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        output_dict['detection_boxes'],
+        output_dict['detection_classes'],
+        output_dict['detection_scores'],
+        category_index,
+        instance_masks=output_dict.get('detection_masks'),
+        use_normalized_coordinates=True,
+        line_thickness=8)
+  img = Image.fromarray(image_np.astype('uint8'), 'RGB')
+  img.save('./object_detection/output.jpg')
+  return send_file('output.jpg', mimetype='image/gif')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
+
---
+++ run.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+from object_detection import app
+
+app.run(host="0.0.0.0", debug=True)
