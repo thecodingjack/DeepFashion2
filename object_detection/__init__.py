378afcc45bb8cf8d31199805f690b2d4de31c2fe
Jack Lim <jacklim.tkl@gmail.com>
2018-08-28 21:54:45 -0400

    trial 2

--- object_detection/__init__.py
+++ object_detection/__init__.py
@@ -41,14 +41,6 @@ PATH_TO_FROZEN_GRAPH = '/Users/tzekeonglim/tensorflow-for-poets-2/DeepFashionV2.
 PATH_TO_LABELS = '/Users/tzekeonglim/tensorflow-for-poets-2/DeepFashionV2.0/data/label_map.pbtxt'
 NUM_CLASSES = 27
 
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
-    serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
-
 # label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 # categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 # category_index = label_map_util.create_category_index(categories)
@@ -123,6 +115,13 @@ def index():
 
 @app.route('/send', methods=['POST'])
 def send():
+  detection_graph = tf.Graph()
+  with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+      with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
 #   arr = json.loads(request.data)['image']['data']['data']
 #   filename = json.loads(request.data)['image']['name']
 #   f = open(os.path.join('./uploads',filename), 'wb')
@@ -130,11 +129,9 @@ def send():
 #   f.close()
 
 #   file_name = os.path.join('./uploads',filename)
-  print('hello')
   image = request.files['image']
   filename = secure_filename(image.filename)
   image.save(os.path.join('./uploads', filename))
-  print(1)
 #   image_path = TEST_IMAGE_PATHS[0]
   image_path = os.path.join('./uploads',filename)
   print(image_path)
